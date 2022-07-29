2d920bafaddc3b54482b14ebbb3535ddafe2ea70
Colaborador <colaborador@N095151.local>
2022-07-29 16:45:23 -0300

    Import Project

---
+++ src/main/java/rian/demo/kafka/Movie.java
@@ -0,0 +1,17 @@
+package rian.demo.kafka;
+
+public class Movie {
+
+	public int id;
+	public String name;
+	public String director;
+	public String genre;
+
+	public Movie(int id, String name, String director, String genre) {
+		this.id = id;
+		this.name = name;
+		this.director = director;
+		this.genre = genre;
+	}
+
+}
---
+++ src/main/java/rian/demo/kafka/PlayedMovie.java
@@ -0,0 +1,12 @@
+package rian.demo.kafka;
+
+public class PlayedMovie {
+
+	public int id;
+	public long duration;
+
+	public PlayedMovie(int id, long duration) {
+		this.id = id;
+		this.duration = duration;
+	}
+}
